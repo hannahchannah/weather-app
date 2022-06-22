8a24bae631f40e24c09df2f6d5896bc1f0b2d6a2
hannahchannah <107792550+hannahchannah@users.noreply.github.com>
2022-06-22 22:33:06 +1000

    Update Icons and Forecast

--- SRC/index.js
+++ SRC/index.js
@@ -1,10 +1,10 @@
 function locationDate(timestamp) {
   let date = new Date(timestamp);
-  let hours = timestamp.getHours();
+  let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
-  let minutes = timestamp.getMinutes();
+  let minutes = date.getMinutes();
   if (minutes < 10) {
     minutes = `0${minutes}`;
   }
@@ -21,6 +21,14 @@ function locationDate(timestamp) {
   let day = days[date.getDay()];
 
   return `${day} ${hours}:${minutes}`;
+}
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
 }
 
 function displayForecast(response) {
@@ -72,15 +80,19 @@ function displayWeather(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind-speed");
+  let dateElement = document.querySelector("#location-date");
   let iconElement = document.querySelector("#icon");
 
   locationElement.innerHTML = response.data.name;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = response.data.wind.speed;
-  iconElement.setAttribute("src"),
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDay(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
   getForecast(response.data.coord);
